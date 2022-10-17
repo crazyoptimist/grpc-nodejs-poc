1d77f67eaf1ed86b7b9e2b5c3413027656db7360
crazyoptimist <crazyoptimist@mail.com>
2022-10-17 01:53:54 -0500

    feat: add greet service to the server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
+var greets = require('../server/proto/greet_pb')
+var service = require('../server/proto/greet_grpc_pb')
+
 var grpc = require('grpc')
 
+/*
+ * Implements the greet RPC method
+ */
+
+function greet(call, callback) {
+  var greeting = new greets.GreetResponse()
+
+  greeting.setResult(
+    "hello " + call.getGreeting().getFirstname()
+  )
+
+  callback(null, greeting)
+}
 
 function main() {
   var server = new grpc.Server()
+  server.addService(service.GreetServiceService, {greet: greet})
 
   server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
   server.start()
