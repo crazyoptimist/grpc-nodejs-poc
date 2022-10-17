a7971427f5eaf4bd3c4c7e2db390753d9ac45987
crazyoptimist <crazyoptimist@mail.com>
2022-10-17 02:34:27 -0500

    feat: add greet client

--- client/client.js
+++ client/client.js
@@ -1,14 +1,30 @@
-var grpc = require('grpc')
+var greets = require('../server/proto/greet_pb')
+var service = require('../server/proto/greet_grpc_pb')
 
-var services = require('../server/proto/dummy_grpc_pb')
+var grpc = require('grpc')
 
 
 function main() {
 
-  var client = new services.DummyServiceClient(
+  var client = new service.GreetServiceClient(
     'localhost:50051',
     grpc.credentials.createInsecure()
   )
 
+  var request = new greets.GreetRequest()
+
+  var greeting = new greets.Greeting()
+  greeting.setFirstName('Jerry')
+  greeting.setLastName('Tom')
+
+  request.setGreeting(greeting)
+
+  client.greet(request, (err, res) => {
+    if (!err) {
+      console.log(res.getResult())
+    } else {
+      console.error(err)
+    }
+  })
 }
 main()
